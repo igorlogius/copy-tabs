030cd622dc335b759d33eb693b115dedce045515
igorlogius <67047467+igorlogius@users.noreply.github.com>
2022-06-16 18:37:05 +0200

    chore: refactor & cleanup

--- background.js
+++ background.js
@@ -1,23 +1,24 @@
 /*global browser */
 
-async function onMessage(data /*, sender*/) {
+async function onMessage(/*data , sender*/) {
     try {
-        let tmp  = await browser.tabs.query(data);
+        const data = {currentWindow: true, active: true};
+        const tabs  = await browser.tabs.query(data);
+        const tab = tabs[0];
 
-        const tab = tmp[0];
-        var link = document.createElement('a');
-        link.href = tab.url;
-        link.textContent = tab.title;
-        document.body.prepend(link);
-        link.focus();
+        let anchor = document.createElement('a');
+        anchor.href = tab.url;
+        anchor.textContent = tab.title;
+        document.body.prepend(anchor);
+        anchor.focus();
         document.getSelection().removeAllRanges();
         var range = document.createRange();
-        range.selectNode(link);
+        range.selectNode(anchor);
         document.getSelection().addRange(range);
         document.execCommand("copy");
-        link.remove();
+        anchor.remove();
 
-        return Promise.resolve("ok");
+        return Promise.resolve(true);
     }catch(e){
         console.error(e);
     }
--- popup.js
+++ popup.js
@@ -1,15 +1,13 @@
 /* global browser */
 async function onLoad() {
     try {
-        const status = await browser.runtime.sendMessage({currentWindow: true, active: true});
-        if(status === 'ok'){
-            const msg = document.getElementById('msg');
-            const body = document.querySelector('body');
-            msg.innerText = "Copied URL";
-            body.style.backgroundColor = 'lightgreen';
-            setTimeout(window.close, 600);
-            return;
-        }
+        const msg = document.getElementById('msg');
+        const body = document.querySelector('body');
+        await browser.runtime.sendMessage({});
+        msg.innerText = " Copied URL ";
+        body.style.backgroundColor = 'lightgreen';
+        setTimeout(window.close, 600);
+        return;
     }catch(e){
         console.error(e);
     }
