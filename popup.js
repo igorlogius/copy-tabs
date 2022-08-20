d2d92acfe059dcf2cd01e119663903be815de2ac
igorlogius <67047467+igorlogius@users.noreply.github.com>
2022-08-20 16:07:21 +0200

    update workflow + cleanup + add multi url copy

--- popup.js
+++ popup.js
@@ -4,15 +4,36 @@ async function onLoad() {
     const msg = document.getElementById('msg');
     const body = document.querySelector('body');
     try {
-        await browser.runtime.sendMessage({});
-        msg.innerText = " Copied URL ";
+        const data = {currentWindow: true, highlighted: true};
+        const tabs  = await browser.tabs.query(data);
+
+        let ul = document.getElementById('list');
+
+        for(const t of tabs) {
+            let br = document.createElement('br');
+            let a = document.createElement('a');
+            a.href = t.url;
+            a.textContent = t.title;
+            ul.append(a);
+            ul.append(br);
+        }
+
+        ul.focus();
+        document.getSelection().removeAllRanges();
+        var range = document.createRange();
+        range.selectNode(ul);
+        document.getSelection().addRange(range);
+        document.execCommand("copy");
+        ul.remove();
+
+        msg.innerText = " Copied URLs of Selected Tabs ";
         body.style.backgroundColor = 'lightgreen';
+        setTimeout(window.close, 1600);
     }catch(e){
-        //console.error(e);
-        msg.innerText = " Error: " + e.toString();
+        console.error(e);
         body.style.backgroundColor = 'red';
+        msg.innerText = " Failed to copy URLs: " + e.toString();
     }
-    setTimeout(window.close, 600);
 }
 
 document.addEventListener('DOMContentLoaded', onLoad);
