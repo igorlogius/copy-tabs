056365c7394875c21c5d5c03ba9167a12e1c8a8a
igorlogius <67047467+igorlogius@users.noreply.github.com>
2023-01-08 17:26:22 +0100

    feat: overhaul usage - removes popup, now redundant + adds context actions + add option to change default button actoin + refactor/simplify code

--- background.js
+++ background.js
@@ -3,22 +3,13 @@
 const manifest = browser.runtime.getManifest();
 const extname = manifest.name;
 
-let singleActionMode = false;
-let singleActionAction = 'cpyalltxt';
+let toolbarAction = 'cpyalllnk';
 let showNotifications = true;
 let ready = false;
-
-const mfest = browser.runtime.getManifest();
 
 async function getFromStorage(type, id, fallback) {
     let tmp = await browser.storage.local.get(id);
     return (typeof tmp[id] === type) ? tmp[id] : fallback;
-}
-
-async function setToStorage(id, value) {
-    let obj = {};
-    obj[id] = value
-    return browser.storage.local.set(obj);
 }
 
 function notify(title, message = "", iconUrl = "icon.png") {
@@ -77,47 +68,26 @@ async function onCommand(cmd) {
 	if(!ready){
 		return;
 	}
-	let qryObj, tabs, ret = false;
-	// action  cpy|sav
-	// amount  sel|all
-	// output  txt|htm
+	console.debug(cmd, showNotifications);
+	let qryObj = { hidden: false, currentWindow: true }, tabs, ret = false;
 	switch(cmd){
-		case 'cpyallhtm':
-			qryObj = {
-			    hidden:false,
-			    currentWindow:true
-			};
+		case 'cpyalllnk':
 			tabs  = await browser.tabs.query(qryObj);
 			ret = copyTabsAsHtml(tabs);
 			break;
 		case 'cpyalltxt':
-			qryObj = {
-			    hidden:false,
-			    currentWindow:true
-			};
 			tabs  = await browser.tabs.query(qryObj);
 			ret = copyTabsAsText(tabs);
 			break;
-		case 'cpyselhtm':
-			qryObj = {
-			    highlighted: true,
-			    hidden:false,
-			    currentWindow:true
-			};
+		case 'cpysellnk':
+			qryObj['highlighted'] = true;
 			tabs  = await browser.tabs.query(qryObj);
 			ret = copyTabsAsHtml(tabs);
 			break;
 		case 'cpyseltxt':
-			qryObj = {
-			    highlighted: true,
-			    hidden:false,
-			    currentWindow:true
-			};
+			qryObj['highlighted'] = true;
 			tabs  = await browser.tabs.query(qryObj);
 			ret = copyTabsAsText(tabs);
-			break;
-		default:
-			console.error('unknown command', cmd);
 			break;
 	}
 	if(ret){
@@ -128,61 +98,61 @@ async function onCommand(cmd) {
 
 		const nid = await notify(extname, manifest.commands[cmd].description.substr(5));
 		if(nid > -1){
-		setTimeout( () => {
-			browser.notifications.clear(nid);
-		},3000); // hide notification after 3 seconds
+			setTimeout( () => {
+				browser.notifications.clear(nid);
+			},3000); // hide notification after 3 seconds
 		}
 	}
 	return ret;
 }
 
-async function onStorageChange(/*changes, area*/) {
+async function onStorageChange() {
 
-  	singleActionMode = await getFromStorage('boolean', 'singleActionMode', false);
-  	singleActionAction = await getFromStorage('string', 'singleActionAction', 'cpyalltxt');
+  	toolbarAction = await getFromStorage('string', 'toolbarAction', 'cpyalllnk'); 
 	showNotifications = await getFromStorage('boolean', 'showNotifications', true);
-	
-	browser.menus.removeAll();
 
-	if(singleActionMode){
-		browser.browserAction.setPopup({ popup: "" });
-		browser.browserAction.setTitle({ title: mfest.commands[singleActionAction].description });
-		for(const cmd of Object.keys(mfest.commands)){
-			browser.menus.create({
-			  type: "radio",
-			  id: ""+cmd,
-			  checked: (cmd === singleActionAction),
-			  title: mfest.commands[cmd].description,
-			  contexts: ["browser_action"],
-			  onclick: (info/*,tab*/) => {
-				singleActionAction = info.menuItemId;		  
-				setToStorage('singleActionAction', singleActionAction);
-			  }
-			});
-		}
-	}else{
-		 browser.browserAction.setPopup({ popup: "popup.html" });
-	}
+	browser.browserAction.setTitle({ title: manifest.commands[toolbarAction].description });
+
+	let txt = toolbarAction[3] +''+ toolbarAction[6];
+	txt = txt.toUpperCase();
+	browser.browserAction.setBadgeText({text: txt });
 }
 
 (async ()=>  {
+
+	// add the 4 context entries 
+	for(const cmd of Object.keys(manifest.commands)){
+		browser.menus.create({
+			id: ""+cmd,
+			title: manifest.commands[cmd].description,
+			contexts: ["browser_action"],
+			onclick: (info) => {
+				onCommand(info.menuItemId);
+			}
+		});
+	}
+
 	await onStorageChange();
 	ready = true;
 })();
 
 browser.storage.onChanged.addListener(onStorageChange);
-
 browser.commands.onCommand.addListener(onCommand);
 
-browser.runtime.onMessage.addListener(
-    (data /*, sender*/) => {
-	  if(typeof data.cmd === 'string'){
-		return Promise.resolve(onCommand(data.cmd));
-	  }
-	  return false;
+// proxy toolbar button click 
+browser.browserAction.onClicked.addListener( () => {
+	onCommand(toolbarAction);
 });
 
-browser.browserAction.onClicked.addListener( () => {
-	onCommand(singleActionAction);
+// add some slight transparancy
+browser.browserAction.setBadgeBackgroundColor({color: [0,0,0, 115]});
+
+// show the user the options page on first installation
+browser.runtime.onInstalled.addListener( (details) => {
+	if(details.reason === "install"){
+		browser.tabs.create({
+			url: "options.html"
+		});
+	}
 });
 
--- options.js
+++ options.js
@@ -8,7 +8,7 @@ function onChange(evt) {
 	let value = ( (el.type === 'checkbox') ? el.checked : el.value)
 	let obj = {}
 
-	//console.log(id,value, el.type,el.min);
+	console.log(id,value, el.type,el.min);
 	if(value === ""){
 		return;
 	}
@@ -33,27 +33,48 @@ function onChange(evt) {
 
 }
 
-[ "showNotifications", "singleActionMode" ].map( (id) => {
+function onLoad(){
 
-	browser.storage.local.get(id).then( (obj) => {
+	const manifest = browser.runtime.getManifest();
+	const toolbarActionSelect = document.getElementById('toolbarAction');
+	for(const cmd of Object.keys(manifest.commands)){
+		toolbarActionSelect.add(new Option(manifest.commands[cmd].description, cmd));
+	}
+
+	[ "showNotifications", "toolbarAction" ].map( (id) => {
+
+		browser.storage.local.get(id).then( (obj) => {
+
+			let el = document.getElementById(id);
+			let val = obj[id];
+
+			if(typeof val !== 'undefined') {
+				if(el.type === 'checkbox') {
+					el.checked = val;
+				}
+				else{
+					el.value = val;
+				}
+			}else{
+				el.value = (() => {
+					switch(id){
+						case 'showNotifications': 
+							return true;
+						case 'toolbarAction': 
+							return 'cpyalllnk';
+						default:
+							return 0;
+					}
+				})();
+			}
+
+		}).catch( console.error );
 
 		let el = document.getElementById(id);
-		let val = obj[id];
+		el.addEventListener('input', onChange);
+	});
 
-		if(typeof val !== 'undefined') {
-			if(el.type === 'checkbox') {
-				el.checked = val;
-			}
-			else{
-				el.value = val;
-			}
-		}else{
-            el.value = 0;
-        }
+}
 
-	}).catch( console.error );
-
-	let el = document.getElementById(id);
-	el.addEventListener('input', onChange);
-});
+document.addEventListener('DOMContentLoaded', onLoad);
 
