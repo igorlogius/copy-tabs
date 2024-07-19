a1ee7a1c22127c83725c9eefa850c43988e72808
igorlogius <67047467+igorlogius@users.noreply.github.com>
2024-07-19 19:19:18 +0200

    feat: add tips to popup

--- popup.js
+++ popup.js
@@ -1,6 +1,24 @@
 /* global browser */
 
+const tipCollection = [
+  "The active tab is always selected",
+  "Hold CTRL to select addional tabs",
+  "Hold SHIFT to select a range of tabs",
+  "You can set custom shortcuts for all copy actions",
+  "All actions are can also be found in the tab context menu",
+  "You can disable this popup on the addons preference page",
+  "Use the 3rd/middle mouse button to invoke the direct action",
+  "Set the direct copy action in the toolbar buttons context menu",
+  "Give the developer some feedback so he'll know whats good and what needs impovement",
+];
+
 function onDOMContentLoaded() {
+  const tip = document.getElementById("tip");
+
+  const text = tipCollection[Math.floor(Math.random() * tipCollection.length)];
+
+  tip.innerText = text;
+
   const manifest = browser.runtime.getManifest();
   const btncontainer = document.getElementById("btncontainer");
 
